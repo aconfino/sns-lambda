27032f6a925c498790f20cfaec29116b853b0fe5
AaronTheDeveloper-at-406918538940 <aaronjeffries@gmail.com>
2018-11-28 08:16:47 -0500

    removed getObject

--- src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
+++ src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
@@ -3,14 +3,10 @@ package com.confino.lambda.codepipeline;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.event.S3EventNotification;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
 public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
     
-    private AmazonS3 amazonS3;
 
     @Override
     public String handleRequest(S3Event event, Context context) {
@@ -20,8 +16,7 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
             String s3Key = record.getS3().getObject().getKey();
             String s3Bucket = record.getS3().getBucket().getName();
             context.getLogger().log("Received record with bucket: " + s3Bucket + "  and key: " + s3Key);
-            S3Object object = amazonS3.getObject(new GetObjectRequest(s3Bucket, s3Key));
-            context.getLogger().log("Retrieved s3 object: " + object);
+            
         }
     	
         return "success!";
