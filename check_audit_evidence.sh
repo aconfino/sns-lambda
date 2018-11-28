7db053b5c1507162c3e3c79ea9a01f2478993f7b
aconfino <aconfino@gmail.com>
2018-11-28 09:58:07 -0500

    check_audit_evidence.sh
    init

---
+++ check_audit_evidence.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Quick script for a POC, nothing more
+
+if [ -f target/surefire-reports/*.xml ]
+then
+	echo "Found evidence.  Proceed!"
+else
+	echo "Missing evidence"
+	exit 1
+fi
