51189e936f2ca74d547bf72af2e3c2e3356bdc8e
AaronTheDeveloper-at-406918538940 <aaronjeffries@gmail.com>
2018-11-28 14:07:05 -0500

    checking for zero records

--- src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
+++ src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
@@ -1,15 +1,22 @@
 package com.confino.lambda.codepipeline;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNS;
 
 public class LambdaFunctionHandler implements RequestHandler<SNSEvent, Object> {
-    public Object handleRequest(SNSEvent request, Context context){
-    context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
-    return null;
-    }
+    
+	public Object handleRequest(SNSEvent request, Context context){
+    	if (request.getRecords().size() > 0) {
+    		SNS sns = request.getRecords().get(0).getSNS();
+    		if (sns.getMessage() != null) {
+    			context.getLogger().log(sns.getMessage());
+    		} else {
+    			context.getLogger().log("No message. " + sns.getSignature() + " " + sns.getSubject());
+    		}
+    	}
+    	return null;
+	}
+
 } 
