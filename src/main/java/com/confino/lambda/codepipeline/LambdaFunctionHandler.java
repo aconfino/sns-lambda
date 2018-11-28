e7ce810b4183c612ba0b93a356aaf4b1032d059d
AaronTheDeveloper-at-406918538940 <aaronjeffries@gmail.com>
2018-11-28 13:37:18 -0500

    Removed timestamp

--- src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
+++ src/main/java/com/confino/lambda/codepipeline/LambdaFunctionHandler.java
@@ -9,12 +9,7 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 
 public class LambdaFunctionHandler implements RequestHandler<SNSEvent, Object> {
     public Object handleRequest(SNSEvent request, Context context){
-    String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-    context.getLogger().log("Invocation started: " + timeStamp);
     context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
-
-    timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-    context.getLogger().log("Invocation completed: " + timeStamp);
-           return null;
-     }
+    return null;
+    }
 } 
